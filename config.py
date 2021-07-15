c0ee5885ff72a30f908d30b217401e0e107d9969
CYBERDUDE3 <85865892+CYBERDUDE3@users.noreply.github.com>
2021-07-15 02:46:33 -0700

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@
 import os
 
 class Config(object):
-    MT_BOT_TOKEN = os.environ.get("MT_BOT_TOKEN", "")
+    MT_BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     API_ID = int(os.environ.get("APP_ID", 12345))
     API_HASH = os.environ.get("API_HASH", "")
-    CAPTION = os.environ.get("CAPTION", "@Mo_Tech_YT @Mo_Tech_Group")
+    CAPTION = os.environ.get("CAPTION", "@M_STER_TECH")
     BUTTON_TEXT = os.environ.get("BUTTON", "🔻Join Channel🔻")
-    URL_LINK = os.environ.get("LINK", "T.ME/MO_TECH_YT")
+    URL_LINK = os.environ.get("LINK", "T.ME/M_STER_TECH")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
